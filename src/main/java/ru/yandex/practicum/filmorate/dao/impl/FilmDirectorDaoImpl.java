00a44036fc3c7e3b94c14fcf27189f3044e4a279
Rustem <lifelikechoco@yandex.ru>
2023-03-14 17:44:26 +0300

    feat: Добавить Поиск, Добавление режиссёров в фильмы

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDirectorDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDirectorDaoImpl.java
@@ -22,21 +22,21 @@ import java.util.List;
 @Component
 public class FilmDirectorDaoImpl implements FilmDirectorDao {
 
-    String SQL_ADD_DIRECTOR = "INSERT INTO director(name) VALUES (?)";
+    private final static String SQL_ADD_DIRECTOR = "INSERT INTO director(name) VALUES (?)";
 
-    String SQL_UPDATE_DIRECTOR = "UPDATE director SET name = ? WHERE id = ?";
+    private final static String SQL_UPDATE_DIRECTOR = "UPDATE director SET name = ? WHERE id = ?";
 
-    String SQL_DELETE_DIRECTOR = "DELETE FROM director WHERE id = ?";
+    private final static String SQL_DELETE_DIRECTOR = "DELETE FROM director WHERE id = ?";
 
-    String SQL_FIND_DIRECTOR_BY_ID = "SELECT * FROM director WHERE id = ?";
+    private final static String SQL_FIND_DIRECTOR_BY_ID = "SELECT * FROM director WHERE id = ?";
 
-    String SQL_FIND_ALL_DIRECTORS = "SELECT * FROM director ORDER BY id ASC";
+    private final static String SQL_FIND_ALL_DIRECTORS = "SELECT * FROM director ORDER BY id ASC";
 
-    String SQL_INSERT_FILM_DIRECTOR = "INSERT INTO film_director (film_id, director_id) VALUES (?, ?)";
+    private final static String SQL_INSERT_FILM_DIRECTOR = "INSERT INTO film_director (film_id, director_id) VALUES (?, ?)";
 
-    String SQL_DELETE_ROW_FILM_DIRECTOR = "DELETE FROM film_director WHERE film_id = ?";
+    private final static String SQL_DELETE_ROW_FILM_DIRECTOR = "DELETE FROM film_director WHERE film_id = ?";
 
-    String SQL_GET_DIRECTOR_BY_FILM = "SELECT * FROM DIRECTOR d " +
+    private final static String SQL_GET_DIRECTOR_BY_FILM = "SELECT * FROM DIRECTOR d " +
             "INNER JOIN FILM_DIRECTOR FD ON d.ID = FD.DIRECTOR_ID WHERE FILM_ID = ?";
 
     private final JdbcTemplate jdbcTemplate;
@@ -52,11 +52,13 @@ public class FilmDirectorDaoImpl implements FilmDirectorDao {
             return stmt;
         }, keyHolder);
         director.setId(keyHolder.getKey().longValue());
+        log.debug("Добавлен новый режисер=[{}]", director);
 
         return director;
     }
     @Override
     public void deleteDirector(Long directorId) {
+        log.debug("Режессер с ID={} был удален.", directorId);
         jdbcTemplate.update(SQL_DELETE_DIRECTOR, directorId);
     }
 
@@ -67,11 +69,13 @@ public class FilmDirectorDaoImpl implements FilmDirectorDao {
         if (check == 0) {
             throw new DirectorNotFoundException(String.format("Режиссер с ID=%d не существует", director.getId()));
         }
+        log.debug("Обновлен режессер с ID={}.", director.getId());
         return findDirectorById(director.getId());
     }
 
     @Override
     public Director findDirectorById(Long directorId) {
+        log.debug("Запрошен режессер с ID={}.", directorId);
         return jdbcTemplate.query(SQL_FIND_DIRECTOR_BY_ID, this::makeDirector, directorId)
                 .stream()
                 .findFirst()
@@ -81,17 +85,20 @@ public class FilmDirectorDaoImpl implements FilmDirectorDao {
 
     @Override
     public List<Director> findAllDirectors() {
+        log.debug("Запрошен список всех режессеров.");
         return jdbcTemplate.query(SQL_FIND_ALL_DIRECTORS, this::makeDirector);
     }
 
 
     @Override
     public void insertToFilmDirector(Long filmId, Long directorId) {
+        log.debug("Вставлена запись в таблицу film_director.");
         jdbcTemplate.update(SQL_INSERT_FILM_DIRECTOR, filmId, directorId);
     }
 
     @Override
     public void deleteRowFromFilmDirector(Long filmId) {
+        log.debug("Удалена запись из таблицы film_director.");
         jdbcTemplate.update(SQL_DELETE_ROW_FILM_DIRECTOR, filmId);
     }
 
